9016394eb978c735cfffbee622a73b0057044bfb
kazuma-0312 <74850915+kazuma-0312@users.noreply.github.com>
2021-01-08 17:14:21 +0900

    テーブルの作成

--- db/migrate/20210108081038_create_tweets.rb
+++ db/migrate/20210108081038_create_tweets.rb
@@ -1,7 +1,10 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-
+      t.string   :title,   null: false
+      t.string   :youtube_url, null: false
+      t.text   :text,      null: false
+      t.references   :user,         foreign_key: true
       t.timestamps
     end
   end
