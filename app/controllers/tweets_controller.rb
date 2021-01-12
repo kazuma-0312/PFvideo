3151c81887beb57d052e44dd6c2a81e5a143727a
kazuma-0312 <74850915+kazuma-0312@users.noreply.github.com>
2021-01-12 18:11:16 +0900

    コントローラーの記述

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -24,6 +24,19 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
   end
 
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    if current_user.id == @tweet.user_id
+      if @tweet.destroy
+        redirect_to root_path
+      else
+        render :show, alert: "Failed to delete"
+      end
+    else
+      redirect_to root_path
+    end
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:title, :youtube_url, :text).merge(user_id: current_user.id)
