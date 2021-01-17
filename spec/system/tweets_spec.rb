0be534a6c6e8a52771ea43927f974192dc499e10
kazuma-0312 <74850915+kazuma-0312@users.noreply.github.com>
2021-01-18 01:16:18 +0900

    ツイート投稿結合テストコード

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -1,9 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe "Tweets", type: :system do
+RSpec.describe 'ツイート投稿', type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.create(:user)
+    @tweet_text = Faker::Lorem.sentence
+    @tweet_title = Faker::Lorem.sentence
+    @tweet_youtube_url = "https://www.youtube.com/watch?v=oyqqcgpWzsM"
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context 'ツイート投稿ができるとき'do
+    it 'ログインしたユーザーは新規投稿できる' do
+      # ログインする
+      sign_in(@user)
+      # 新規投稿ページへのリンクがあることを確認する
+      expect(page).to have_content('New Tweet')
+      # 投稿ページに移動する
+      visit new_tweet_path
+      # フォームに情報を入力する
+      fill_in 'tweet-title', with: @tweet_title
+      fill_in 'tweet-text', with: @tweet_text
+      fill_in 'tweet-url', with: @tweet_youtube_url
+      # 送信するとTweetモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Tweet.count }.by(1)
+      # トップページに遷移する
+      visit root_path
+      # トップページには先ほど投稿した内容のツイートが存在することを確認する（タイトル）
+      expect(page).to have_content(@tweet_title)
+      # トップページには先ほど投稿した内容のツイートが存在することを確認する（テキスト）
+      expect(page).to have_content(@tweet_text)
+    end
+  end
+  context 'ツイート投稿ができないとき'do
+    it 'ログインしていないと新規投稿ページに遷移できない' do
+      # トップページに遷移する
+      visit root_path
+      # 新規投稿ページへのリンクがない
+      expect(page).to have_no_content('New Tweet')
+    end
+  end
 end
