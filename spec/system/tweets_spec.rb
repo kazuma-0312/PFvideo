4e2b69bdb5c644951174ffb33b42919711827259
kazuma-0312 <74850915+kazuma-0312@users.noreply.github.com>
2021-01-18 01:32:46 +0900

    ツイートの編集結合テスト

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -16,9 +16,9 @@ RSpec.describe 'ツイート投稿', type: :system do
       # 投稿ページに移動する
       visit new_tweet_path
       # フォームに情報を入力する
-      fill_in 'tweet-title', with: @tweet_title
-      fill_in 'tweet-text', with: @tweet_text
-      fill_in 'tweet-url', with: @tweet_youtube_url
+      fill_in 'tweet_title', with: @tweet_title
+      fill_in 'tweet_text', with: @tweet_text
+      fill_in 'tweet_url', with: @tweet_youtube_url
       # 送信するとTweetモデルのカウントが1上がることを確認する
       expect{
         find('input[name="commit"]').click
@@ -37,6 +37,51 @@ RSpec.describe 'ツイート投稿', type: :system do
       visit root_path
       # 新規投稿ページへのリンクがない
       expect(page).to have_no_content('New Tweet')
+    end
+  end
+end
+
+RSpec.describe 'ツイート編集', type: :system do
+  before do
+    @tweet1 = FactoryBot.create(:tweet)
+    @tweet2 = FactoryBot.create(:tweet)
+  end
+  context 'ツイート編集ができるとき' do
+    it 'ログインしたユーザーは自分が投稿したツイートの編集ができる' do
+      # ツイート1を投稿したユーザーでログインする
+      sign_in(@tweet1.user)
+      # 編集ページへ遷移する
+      visit edit_tweet_path(@tweet1)
+      # すでに投稿済みの内容がフォームに入っていることを確認する
+      expect(
+        find('#tweet_title').value
+      ).to eq @tweet1.title
+      expect(
+        find('#tweet_text').value
+      ).to eq @tweet1.text
+      # 投稿内容を編集する
+      fill_in 'tweet_title', with: "#{@tweet1.title}+編集したタイトル"
+      fill_in 'tweet_text', with: "#{@tweet1.text}+編集したテキスト"
+      # 編集してもTweetモデルのカウントは変わらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Tweet.count }.by(0)
+      # トップページに遷移する
+      visit root_path
+      # トップページには先ほど変更した内容のツイートが存在することを確認する（タイトル）
+      expect(page).to have_content("#{@tweet1.title}+編集したタイトル")
+      # トップページには先ほど変更した内容のツイートが存在することを確認する（テキスト）
+      expect(page).to have_content("#{@tweet1.text}+編集したテキスト")
+    end
+  end
+  context 'ツイート編集ができないとき' do
+    it 'ログインしたユーザーは自分以外が投稿したツイートの編集画面には遷移できない' do
+      # ツイート1を投稿したユーザーでログインする
+      sign_in(@tweet1.user)
+    end
+    it 'ログインしていないとツイートの編集画面には遷移できない' do
+      # トップページにいる
+      visit root_path
     end
   end
 end
