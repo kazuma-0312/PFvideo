d4f10e723f6b16f7163848145f2f6695ce3551c2
Kazuma Sakamoto <74850915+kazuma-0312@users.noreply.github.com>
2021-02-12 22:13:47 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'comments/new'
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   root to: "tweets#index"
 
   resources :tweets do
